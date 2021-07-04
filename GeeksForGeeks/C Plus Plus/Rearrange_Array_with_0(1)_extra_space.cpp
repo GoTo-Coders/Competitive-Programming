de92ada272ed5935d58a2adc04c139eb234bdcef
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-07-04 23:25:34 +0530

    first commit-Ankit

--- GeeksForGeeks/C Plus Plus/Rearrange_Array_with_0(1)_extra_space.cpp
+++ GeeksForGeeks/C Plus Plus/Rearrange_Array_with_0(1)_extra_space.cpp
@@ -1,6 +1,6 @@
 /*
 Problem Statement:
-------------------
+-----------------
 Given an array arr[] of size N where every element is in the range from 0 to n-1. Rearrange the given array so that arr[i] becomes arr[arr[i]].
 
 Example 1:
