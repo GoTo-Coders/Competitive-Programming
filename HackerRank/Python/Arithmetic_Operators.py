15e99daa899958792986bea244b1bcd721af2121
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-11-04 18:40:46 +0530

    Create Arithmetic_Operators.py

---
+++ HackerRank/Python/Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+// Link --> https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=false
+
+// Code:
+def operatre(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    operatre(a, b)
