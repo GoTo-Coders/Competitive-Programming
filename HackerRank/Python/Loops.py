7c06460dd56782c312c37f3e80f59e9611874da9
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-11-05 00:23:58 +0530

    first commit-Sushant

---
+++ HackerRank/Python/Loops.py
@@ -0,0 +1,8 @@
+# Link --> https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+# Code:
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i*i)
