858d34e9db61fb6e9fa0a1c710d6558e98382143
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-11-05 21:34:33 +0530

    first commit-Sushant

--- HackerRank/Python/Find_the_Runner-Up_Score.py
+++ HackerRank/Python/Find_the_Runner-Up_Score.py
@@ -6,4 +6,20 @@ if __name__ == '__main__':
     arr = list(map(int, input().split()))
     arr.sort()
     
+    second_max = 0
+    max_no = max(arr)
+
+    for i in arr:
+        if i > second_max and i < max_no:
+            second_max = i
+
+    print(second_max)
+
+# OTHER WAY:
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    
     print(arr[arr.index(max(arr)) - 1])
