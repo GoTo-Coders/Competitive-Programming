4a4c09b9c060bd7850f7cb7aa5a3ddaea12a04c8
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-11-05 11:20:52 +0530

    first commit-Sushant

---
+++ HackerRank/Python/Python-Print.cpp
@@ -0,0 +1,8 @@
+# Link --> https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+
+# Code:
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(1, n+1):
+        print(i , end="")
