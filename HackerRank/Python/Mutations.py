7dafec5b1bb6450cf3a5fe25c57df7cefcccd7c1
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-11-10 09:33:09 +0530

    first commit-Sushant

---
+++ HackerRank/Python/Mutations.py
@@ -0,0 +1,13 @@
+# Link --> https://www.hackerrank.com/challenges/python-mutations/problem
+
+# Code: 
+def mutate_string(string, position, character):
+    string = string[ :position] + character + string[position+1: ]
+    
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
