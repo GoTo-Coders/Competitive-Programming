2111c0dcb8b7c3866295b498b117e3edf63dc38a
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-12-01 17:53:51 +0530

    first commit-Sushant

---
+++ HackerRank/Python/Text_Alignment.py
@@ -0,0 +1,20 @@
+# Link --> https://www.hackerrank.com/challenges/text-alignment/problem
+
+# Code:
+thickness = int(input()) 
+c = 'H'
+
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
