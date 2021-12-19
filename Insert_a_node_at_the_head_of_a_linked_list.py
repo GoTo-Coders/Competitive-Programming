1a57afdaf8b7d93e60e4665dc452f911beeef2d4
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-12-19 19:03:28 +0530

    first commit-Sushant

---
+++ Insert_a_node_at_the_head_of_a_linked_list.py
@@ -0,0 +1,56 @@
+# Link --> https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list/problem
+
+# Code:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def insertNodeAtHead(head, data):
+    newNode = SinglyLinkedListNode(data)
+    
+    if head:
+        newNode.next = head
+    
+    return newNode
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtHead(llist.head, llist_item)
+        llist.head = llist_head
+    
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+    
+    fptr.close()
+
