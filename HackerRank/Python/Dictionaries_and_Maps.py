81e1d78949354e478d49a96954ba7db228915169
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-15 21:55:03 +0530

    first commit

---
+++ HackerRank/Python/Dictionaries_and_Maps.py
@@ -0,0 +1,20 @@
+# Link --> https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+
+# Code:
+x = int(input())
+
+dictt = {}
+
+for i in range(x):
+    user = input().split()
+    dictt[user[0]] = user[1]
+
+while True:
+    try:
+        query = input()
+        if query in dictt:
+            print(query + "=" + dictt[query])
+        else:
+            print("Not found")
+    except EOFError:
+        break
