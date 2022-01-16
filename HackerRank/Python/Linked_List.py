fd5aff52c50047cec018857b3c018dcfb2625fb9
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-16 11:12:50 +0530

    Create Linked_List.py

---
+++ HackerRank/Python/Linked_List.py
@@ -0,0 +1,17 @@
+# Link --> https://www.hackerrank.com/challenges/30-linked-list/problem
+
+# Code:
+def insert(self,head,data):
+        newNode = Node(data) 
+        # When the list empty
+        if head == None:
+            head = newNode
+        
+        # When there are elements in the list
+        else:
+            current_Node = head
+            while current_Node.next:
+                current_Node = current_Node.next
+            current_Node.next = newNode
+    
+        return head
