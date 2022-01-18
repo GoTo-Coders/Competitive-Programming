70f9cd4904ed49041292e7f54535c7d986dcc528
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-19 00:18:48 +0530

    first commit-Sushant

---
+++ HackerRank/JavaScript/Hello-World!.js
@@ -0,0 +1,8 @@
+// Link --> https://www.hackerrank.com/challenges/js10-hello-world/problem
+
+// Code:
+function greeting(parameterVariable) 
+{
+    console.log('Hello, World!');
+    console.log(parameterVariable) 
+}
