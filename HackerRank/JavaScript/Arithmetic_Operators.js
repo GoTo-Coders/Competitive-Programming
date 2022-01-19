dd85628ef5adc0b12d8a96978b60a690efe76136
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-19 15:59:32 +0530

    first commit-Sushant

---
+++ HackerRank/JavaScript/Arithmetic_Operators.js
@@ -0,0 +1,10 @@
+// Link --> https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+
+// Code:
+function getArea(length, width) {
+    return length * width;
+}
+
+function getPerimeter(length, width) {
+    return 2 * (length + width);
+}
