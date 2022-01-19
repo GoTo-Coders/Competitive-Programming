4b6c800e1a03dfc918422b13fb2c3acf13e8b291
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-19 16:20:45 +0530

    first commit-Sushant

---
+++ HackerRank/JavaScript/Conditional_Statements:If-Else.js
@@ -0,0 +1,20 @@
+// Link --> https://www.hackerrank.com/challenges/js10-if-else/problem
+
+// Code:
+function getGrade(score) {
+    let grade;
+    if(score <= 5)
+        grade = 'F';
+    else if(score <= 10)
+        grade = 'E';
+    else if(score <= 15)
+        grade = 'D';
+    else if(score <= 20)
+        grade = 'C';
+    else if(score <= 25)
+        grade = 'B';
+    else
+        grade = 'A';
+    
+    return grade;
+}
