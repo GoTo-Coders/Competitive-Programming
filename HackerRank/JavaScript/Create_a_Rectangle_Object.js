47b246cf7887335b04ad628037415643503354ce
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-20 17:41:26 +0530

    first commit-Sushant

---
+++ HackerRank/JavaScript/Create_a_Rectangle_Object.js
@@ -0,0 +1,9 @@
+// Link --> https://www.hackerrank.com/challenges/js10-objects/problem
+
+// Code:
+function Rectangle(a, b) {
+    this.length = a;
+    this.width = b;
+    this.perimeter = (2 * (a + b));
+    this.area = (a * b);
+}
