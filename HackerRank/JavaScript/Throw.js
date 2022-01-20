32295f0db6adbc0a6966c9c6c32603cdbfe4391b
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-20 17:33:36 +0530

    first commit-Sushant

---
+++ HackerRank/JavaScript/Throw.js
@@ -0,0 +1,11 @@
+// Link --> https://www.hackerrank.com/challenges/js10-throw/problem
+
+// Code:
+function isPositive(a) {
+    if(a > 0)
+        return 'YES';
+    else if(a == 0)
+        return 'Zero Error';
+    else
+        return 'Negative Error';
+}
