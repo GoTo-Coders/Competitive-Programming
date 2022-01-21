d93a49a16a453b49943fc7ae05daeb605274f91d
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-21 11:37:55 +0530

    first commit-Sushant

---
+++ HackerRank/JavaScript/Regular_Expressions_II.js
@@ -0,0 +1,8 @@
+// Link --> https://www.hackerrank.com/challenges/js10-regexp-2/problem
+
+// Code:
+function regexVar() {
+    const re = new RegExp(/^(Mr\.|Dr\.|Er\.|Ms\.|Mrs\.)\s?[a-z|A-Z]+$/);
+    return re;
+}
+
