0845abd4c71de543da915ced26c7987feecbdd31
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-21 16:03:02 +0530

    first commit-Sushant

---
+++ HackerRank/Python/Weighted_Mean.py
@@ -0,0 +1,12 @@
+# Link --> https://www.hackerrank.com/challenges/s10-weighted-mean/problem
+
+# Code:
+size = int(input())
+numbers = list(map(int, input().split()))
+weighted = list(map(int, input().split()))
+sum_items = 0
+
+for i in range(size):
+    sum_items = sum_items + (numbers[i] * weighted[i])
+    
+print(round(sum_items / sum(weighted), 1))
