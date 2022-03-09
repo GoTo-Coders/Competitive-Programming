63ec796b7951fd36de9f59e6a4fb7c8f38be9d36
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-03-09 18:55:54 +0530

    first commit-Sushant

---
+++ GeeksForGeeks/Python/First_and_last_occurrences_of_X.py
@@ -0,0 +1,34 @@
+"""
+Given a sorted array having N elements, find the indices of the first and last occurrences of an element X in the given array.
+Note: If the number X is not found in the array, return '-1' as an array.
+
+Example 1:
+---------
+Input:
+N = 4 , X = 3
+arr[] = { 1, 3, 3, 4 }
+Output:
+1 2
+Explanation: For the above array, first occurence of X = 3 is at index = 1 and last occurence is at index = 2.
+
+Example 2:
+----------
+Input:
+N = 4, X = 5
+arr[] = { 1, 2, 3, 4 }
+Output:
+-1
+Explanation: As 5 is not present in the array, so the answer is -1.
+
+Your Task: You don't need to read input or print anything. Complete the function firstAndLast() that takes the array arr, its size N and the value X as 
+input parameters and returns a list of integers containing the indices of first and last occurence of X.
+
+Expected Time Complexity: O(log(N))
+Expected Auxiliary Space: O(1)
+"""
+
+# Link --> https://practice.geeksforgeeks.org/problems/first-and-last-occurrences-of-x2041/1/#
+
+# Code:
+
+
