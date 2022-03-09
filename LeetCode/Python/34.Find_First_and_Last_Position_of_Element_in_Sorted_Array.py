8617f6da4c36753893bc2e1683aea878c138e0e9
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-03-09 18:59:15 +0530

    first commit-Sushant

--- LeetCode/Python/34.Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ LeetCode/Python/34.Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -1,30 +1,21 @@
 """
-Given a sorted array having N elements, find the indices of the first and last occurrences of an element X in the given array.
-Note: If the number X is not found in the array, return '-1' as an array.
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1]. You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
 ----------
-Input:
-N = 4 , X = 3
-arr[] = { 1, 3, 3, 4 }
-Output:
-1 2
-Explanation: For the above array, first occurence of X = 3 is at index = 1 and last occurence is at index = 2.
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
 
 Example 2:
 ----------
-Input:
-N = 4, X = 5
-arr[] = { 1, 2, 3, 4 }
-Output:
--1
-Explanation: As 5 is not present in the array, so the answer is -1.
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
 
-Your Task: You don't need to read input or print anything. Complete the function firstAndLast() that takes the array arr, its size N and the value X as input parameters
-and returns a list of integers containing the indices of first and last occurence of X.
-
-Expected Time Complexity: O(log(N))
-Expected Auxiliary Space: O(1)
+Example 3:
+---------
+Input: nums = [], target = 0
+Output: [-1,-1]
 """
 
 # Link --> https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/
