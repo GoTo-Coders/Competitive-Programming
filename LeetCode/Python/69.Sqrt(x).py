4ad425730db964a2602d702483783e7407f01718
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-04-02 18:33:52 +0530

    first commit-Sushant

---
+++ LeetCode/Python/69.Sqrt(x).py
@@ -0,0 +1,69 @@
+"""
+Problem Statement:
+------------------
+Given a non-negative integer x, compute and return the square root of x.
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+Example 1:
+Input: x = 4
+Output: 2
+
+Example 2:
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+"""
+
+# Link --> https://leetcode.com/problems/sqrtx/
+
+# Code:
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # Base cases:
+        if x == 0 or x == 1:
+            return x
+        
+        # Using binary search approach        
+        low = 0
+        high = x//2
+        answer = -1
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            current_sqrt = mid * mid
+            
+            if current_sqrt == x:
+                return mid
+            if current_sqrt < x:
+                answer = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+        return answer
+      
+"""
+# Less optimized:
+# ---------------
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # Using binary search approach        
+        low = 0
+        high = x
+        answer = -1
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            current_sqrt = mid * mid
+            
+            if current_sqrt == x:
+                return mid
+            if current_sqrt < x:
+                answer = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+        return answer
+"""
