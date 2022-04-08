72ba2e91f2913568a11aa0ab581d410c23d20823
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-04-08 20:24:35 +0530

    first commit-Sushant

--- GeeksForGeeks/Python/Find_minimum_and_maximum_element_in_an_array.py
+++ GeeksForGeeks/Python/Find_minimum_and_maximum_element_in_an_array.py
@@ -1,6 +1,6 @@
 '''
 Problem Statement:
------------------
+------------------
 Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
 Example 1:
 ---------
