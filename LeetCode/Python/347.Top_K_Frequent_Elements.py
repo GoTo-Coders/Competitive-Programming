93abae5baf4030e7c49f344f5f9541739cdab793
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-04-09 15:15:59 +0530

    first commit-Sushant

---
+++ LeetCode/Python/347.Top_K_Frequent_Elements.py
@@ -0,0 +1,27 @@
+"""
+Problem Statement:
+------------------
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+"""
+
+# Link --> https://leetcode.com/problems/top-k-frequent-elements/
+
+# Code:
+import collections
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = collections.Counter(nums)
+        
+        answer = []
+        for i in count.most_common(k):
+            answer.append(i[0])
+        
+        return answer
