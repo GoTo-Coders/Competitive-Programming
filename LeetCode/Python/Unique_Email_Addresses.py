3dec94ed25ac221a6e77325a940251f66df16608
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-04-28 21:45:28 +0530

    first commit-Sushant

---
+++ LeetCode/Python/Unique_Email_Addresses.py
@@ -0,0 +1,43 @@
+"""
+Problem Statement:
+------------------
+Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name. If you add periods '.' between some characters in the local 
+name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+
+For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address. If you add a plus '+' in the local name, everything after the 
+first plus sign will be ignored. This allows certain emails to be filtered. Note that this rule does not apply to domain names.
+
+For example, "m.y+name@email.com" will be forwarded to "my@email.com". It is possible to use both of these rules at the same time.
+Given an array of strings emails where we send one email to each emails[i], return the number of different addresses that actually receive mails.
+ 
+Example 1:
+Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+Output: 2
+Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
+
+Example 2:
+Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+Output: 3
+"""
+
+# Link --> https://leetcode.com/problems/unique-email-addresses/
+
+# Code:
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        # initializing an empty set to store correct email addresses as set only contains unique elements.
+        answer = {''}
+
+        for email in emails:
+            # splitting the email into account name and domain name.
+            account, domain = email.split("@")
+
+            # removing the '.' before all the '+' symbol
+            account = account.split("+")[0].replace(".", "")
+
+            # adding the correct email address into the answer set.
+            answer.add(account + "@" + domain)
+
+        return len(answer)-1
+      
