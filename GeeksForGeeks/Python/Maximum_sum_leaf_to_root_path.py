b6b4419e38509df2294d8928ab232a39a6b46ac0
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-07-24 18:21:01 +0530

    first commit-Sushant

---
+++ GeeksForGeeks/Python/Maximum_sum_leaf_to_root_path.py
@@ -0,0 +1,48 @@
+"""
+Problem Statement:
+------------------
+Given a Binary Tree, find the maximum sum path from a leaf to root.
+
+Example 1:
+---------
+Input:
+        1
+       / \
+      2   3 
+Output:
+4
+Explanation: Following the path 3 -> 1, results in a sum of 4, which is the maximum path sum from leaf to root for the given tree.
+
+Example 2:
+----------
+Input:
+       10
+      /  \
+    -2    7
+    / \   
+   8  -4    
+Output:
+17
+Explanation : Following the path 7 -> 10, results in a sum of 17, which is the maximum path sum from leaf to root for the given tree.
+
+Your task : You don't need to read input or print anything. Your task is to complete the function maxPathSum() which takes the root node of the tree 
+as input and returns an integer denoting the maximum possible leaf to root path sum.
+
+Expected Time Complexity: O(n) , where n = number of nodes
+Expected Auxiliary Space: O(1)
+"""
+
+# Link --> https://practice.geeksforgeeks.org/problems/maximum-sum-leaf-to-root-path/1
+
+# Code:
+class Solution:
+    def maxPathSum(self, root):
+        if not root:
+            return 0
+        
+        currentSum = root.data
+        
+        maximum = max(self.maxPathSum(root.left), self.maxPathSum(root.right))
+        
+        return (maximum + root.data)
+
