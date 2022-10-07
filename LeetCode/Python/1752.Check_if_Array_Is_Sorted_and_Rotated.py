69a7fc294f5fe57f93ebf54e17dd90c900f54de4
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-10-07 11:11:23 +0530

    first commit-Sushant

--- LeetCode/Python/1752.Check_if_Array_Is_Sorted_and_Rotated.py
+++ LeetCode/Python/1752.Check_if_Array_Is_Sorted_and_Rotated.py
@@ -1,6 +1,6 @@
 """
 Problem Statement:
-------------------
+-----------------
 Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). 
 Otherwise, return false. There may be duplicates in the original array.
 
